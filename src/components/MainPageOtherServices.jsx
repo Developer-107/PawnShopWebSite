605461e33f0181b01c6160875983088d078d7bf5
Your Name <you@example.com>
2025-09-26 13:34:35 +0400

    correctiononResponsiveCorrect

--- src/components/MainPageOtherServices.jsx
+++ src/components/MainPageOtherServices.jsx
@@ -18,10 +18,10 @@ export default function MainPageOtherServices() {
     <div className="mr-auto xl:text-[22.5px] md:text-[17.9px] text-[16.1px]"><p className="font-bold ">სხვა მომსახურებები</p></div>
     <br/>
 
-    <div className="flex w-[100%] lg:flex-row flex-col lg:gap-0 gap-[22px]">
+    <div className="flex w-[100%] lg:flex-row flex-col lg:gap-0 sm:gap-[22px] gap-[14px]">
 
    
-    <Link to="/rules#pawningItem" className="relative block lg:mr-auto overflow-hidden rounded-lg leaveItemContainer group w-[100%]">
+    <Link to="/rules#pawningItem" className="relative block lg:mr-auto overflow-hidden rounded-lg leaveItemContainer group w-[100%] threeScreenSizeDiv">
   <img
     src={leaveItem}
     alt="Leave item Photo"
@@ -33,15 +33,15 @@ export default function MainPageOtherServices() {
 </Link>
     
 
-    <div className="flex flex-col gap-[22px] w-[100%]">
-    <Link to="/rules#buyingItems" className="relative block lg:mr-auto overflow-hidden rounded-lg leaveItemContainer group h-full lg:!h-[230px] w-[100%]">
+    <div className="flex flex-col sm:gap-[22px] gap-[14px] w-[100%] sm:h-full">
+    <Link to="/rules#buyingItems" className="relative block lg:mr-auto overflow-hidden rounded-lg leaveItemContainer group h-full lg:!h-[230px] w-[100%] threeScreenSizeDiv">
         <img className="leaveItem w-full h-full object-cover transition-transform duration-700 ease-in-out group-hover:scale-107" 
         src={buyItem} alt="Buy item Photo" />
         <div className="absolute bottom-4 left-4 text-[#f2f2f2] xl:text-[17.8px] md:text-[16.7px] font-semibold text-lg z-10">ნივთის ყიდვა</div>
 
         </Link>
     
-    <Link to="/rules#sellingItem" className="relative block lg:mr-auto overflow-hidden rounded-lg leaveItemContainer group h-full lg:!h-[230px] w-full">
+    <Link to="/rules#sellingItem" className="relative block lg:mr-auto overflow-hidden rounded-lg leaveItemContainer group h-full lg:!h-[230px] w-full threeScreenSizeDiv">
         <img className="leaveItem w-full h-full object-cover transition-transform duration-700 ease-in-out group-hover:scale-107" 
         src={sellItem} alt="Sell item Photo" />
         <div className="absolute bottom-4 left-4 text-[] text-[#f2f2f2] xl:text-[17.8px] md:text-[16.7px] font-semibold text-lg z-10">ნივთის გაყიდვა</div>
